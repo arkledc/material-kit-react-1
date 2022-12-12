9f69876793eec052677c8780332b5b12ce243df0
arkledc <118317335+arkledc@users.noreply.github.com>
2022-12-12 00:16:11 +0000

    1 file modified

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -36,7 +36,7 @@ import USERLIST from '../_mock/user';
 const TABLE_HEAD = [
   { id: 'name', label: 'Name', alignRight: false },
   { id: 'company', label: 'Company', alignRight: false },
-  { id: 'role', label: 'Role', alignRight: false },
+  { id: 'role', label: 'Phone', alignRight: false },
   { id: 'isVerified', label: 'Verified', alignRight: false },
   { id: 'status', label: 'Status', alignRight: false },
   { id: '' },
